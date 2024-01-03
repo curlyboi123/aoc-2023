c8f2236ddf1e9f69541473d305fa467089246f33
John Colton <john.colton@accenture.com>
2024-01-03 16:14:46 +0000

    Day 5

--- 05/main.go
+++ 05/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 )
+
+type mapping struct {
+	sourceMin, sourceMax, change int
+}
 
 func getFileContentByLine() []string {
 	filePath := os.Args[1]
@@ -12,12 +18,81 @@ func getFileContentByLine() []string {
 	return strings.Split(string(content), "\n")
 }
 
-func main() {
+func partOne(mappings [][]mapping) {
 	lines := getFileContentByLine()
 	_, seedsStr, _ := strings.Cut(lines[0], ":")
 	seeds := strings.Fields(seedsStr)
-	fmt.Println(seeds)
-	for _, line := range lines {
-		fmt.Println(line)
+	var locationValues []int
+	for _, seed := range seeds {
+		value, _ := strconv.Atoi(seed)
+		for _, mapping := range mappings {
+			for _, mappingRange := range mapping {
+				if value >= mappingRange.sourceMin && value <= mappingRange.sourceMax {
+					value = value + mappingRange.change
+					break
+				}
+			}
+		}
+		locationValues = append(locationValues, value)
 	}
+	fmt.Println(slices.Min(locationValues))
+}
+
+func partTwo(mappings [][]mapping) {
+	lines := getFileContentByLine()
+	_, s, _ := strings.Cut(lines[0], ":")
+	seedRanges := strings.Fields(s)
+
+	var locationValues []int
+
+	for i := 0; i < len(seedRanges)-1; i += 2 {
+		seedMin, _ := strconv.Atoi(seedRanges[i])
+		seedRange, _ := strconv.Atoi(seedRanges[i+1])
+		seedMax := seedMin + seedRange
+		for j := seedMin; j < seedMax; j++ {
+			// fmt.Printf("Calculating mapping: %d/%d\n", j-seedMin, seedMax-seedMin)
+			value := j
+			for _, mapping := range mappings {
+				for _, mappingRange := range mapping {
+					if value >= mappingRange.sourceMin && value <= mappingRange.sourceMax {
+						value = value + mappingRange.change
+						break
+					}
+				}
+			}
+			locationValues = append(locationValues, value)
+		}
+	}
+	fmt.Println(slices.Min(locationValues))
+
+}
+
+func main() {
+	lines := getFileContentByLine()
+
+	// Create mappings
+	var mappings [][]mapping
+	var innerMapping []mapping
+	var mapNames []string
+	for idx, line := range lines[2:] {
+		if strings.Contains(line, "map") {
+			mapName := strings.Fields(line)[0]
+			mapNames = append(mapNames, mapName)
+			continue
+		} else if line != "" {
+			var destinationMin, sourceMin, mappingRange int
+			fmt.Sscanf(line, "%d %d %d", &destinationMin, &sourceMin, &mappingRange)
+			mapping := mapping{sourceMin, sourceMin + mappingRange - 1, destinationMin - sourceMin}
+			innerMapping = append(innerMapping, mapping)
+		}
+		if line == "" || idx == len(lines[2:])-1 {
+			if len(innerMapping) != 0 {
+				mappings = append(mappings, innerMapping)
+			}
+			innerMapping = nil
+		}
+	}
+
+	partOne(mappings)
+	partTwo(mappings)
 }
