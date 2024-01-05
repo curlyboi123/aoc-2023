0a0904f03504f833c38ba8e0dd931e3425b3a65b
John Colton <john.colton@accenture.com>
2024-01-05 16:30:40 +0000

    Go module attempt

---
+++ utils/go.mod
@@ -0,0 +1,3 @@
+module github.com/curlyboi/aoc-2023/utils
+
+go 1.21.4
---
+++ utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"os"
+	"strings"
+)
+
+func getFileContentByLine() []string {
+	filePath := os.Args[1]
+	content, _ := os.ReadFile(filePath)
+	return strings.Split(string(content), "\n")
+}
