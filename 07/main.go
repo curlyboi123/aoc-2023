3ccb506993f2c4dc0d8279a3fdfeadc315bed193
John Colton <john.colton@accenture.com>
2024-01-08 16:07:06 +0000

    Refactored hand ranking into func

--- 07/main.go
+++ 07/main.go
@@ -14,10 +14,62 @@ func getFileContentByLine() []string {
 	return strings.Split(string(content), "\n")
 }
 
-func main() {
+type Rank int64
+
+const (
+	HighCard Rank = iota
+	OnePair
+	TwoPair
+	ThreeOfAKind
+	FullHouse
+	FourOfAKind
+	FiveOfAKind
+)
+
+func getHandRanking(hand string) int {
+	// Count number of each different value card in hand
+	cardCount := make(map[string]int)
+	for _, char := range hand {
+		if _, ok := cardCount[string(char)]; !ok {
+			cardCount[string(char)] = strings.Count(hand, string(char))
+		}
+	}
+	// Potential different approach could be taken by looping through cards
+	// count and increase hand ranking as hands are discovered
+
+	// Frequency of each number of card occurrences
+	cardCountCount := make(map[int]int)
+	for _, v := range cardCount {
+		cardCountCount[v]++
+	}
+
+	// Assign hand rank
+	if _, ok := cardCountCount[5]; ok {
+		return int(FiveOfAKind)
+	} else if _, ok := cardCountCount[4]; ok {
+		return int(FourOfAKind)
+	} else if _, ok := cardCountCount[3]; ok {
+		if _, ok := cardCountCount[2]; ok {
+			return int(FullHouse)
+		} else {
+			return int(ThreeOfAKind)
+		}
+	} else if val, ok := cardCountCount[2]; ok {
+		if val == 2 {
+			return int(TwoPair)
+		} else if val == 1 {
+			return int(OnePair)
+		}
+	} else {
+		return int(HighCard)
+	}
+	return -1
+}
+
+func partOne() {
 	lines := getFileContentByLine()
 
-	itemOrder := map[string]int{
+	cardRankings := map[string]int{
 		"2": 0,
 		"3": 1,
 		"4": 2,
@@ -40,49 +92,16 @@ func main() {
 		handBidMap[hand] = bid
 	}
 
-	// Ordered list to hold the hands. Position in list is relative rank of hand type
+	// Ordered list to hold the hands grouped by rank e.g. FullHouse
+	// Position in list is relative rank of hand type
 	handCount := make([][]string, 7)
 
 	for hand := range handBidMap {
-		// Count number of each card in hand
-		cardCount := make(map[string]int)
-		for _, char := range hand {
-			if _, ok := cardCount[string(char)]; !ok {
-				cardCount[string(char)] = strings.Count(hand, string(char))
-			}
-		}
-		// Potential different approach could be taken by looping through cards
-		// count and increase hand ranking as hands are discovered
-
-		// Frequency of each number of occurrences
-		cardCountCount := make(map[int]int)
-		for _, v := range cardCount {
-			cardCountCount[v]++
-		}
-
-		// Assign hand rank
-		if _, ok := cardCountCount[5]; ok {
-			handCount[6] = append(handCount[6], hand)
-		} else if _, ok := cardCountCount[4]; ok {
-			handCount[5] = append(handCount[5], hand)
-		} else if _, ok := cardCountCount[3]; ok {
-			if _, ok := cardCountCount[2]; ok {
-				handCount[4] = append(handCount[4], hand)
-			} else {
-				handCount[3] = append(handCount[3], hand)
-			}
-		} else if val, ok := cardCountCount[2]; ok {
-			if val == 2 {
-				handCount[2] = append(handCount[2], hand)
-			} else if val == 1 {
-				handCount[1] = append(handCount[1], hand)
-			}
-		} else {
-			handCount[0] = append(handCount[0], hand)
-		}
+		handRank := getHandRanking(hand)
+		handCount[handRank] = append(handCount[handRank], hand)
 	}
 
-	// Rank hands
+	// Order hands by rank
 	for _, hands := range handCount {
 		sort.Slice(hands, func(i, j int) bool {
 			var cond bool
@@ -90,7 +109,7 @@ func main() {
 				if hands[i][n] == hands[j][n] {
 					continue
 				} else {
-					cond = itemOrder[string(hands[i][n])] < itemOrder[string(hands[j][n])]
+					cond = cardRankings[string(hands[i][n])] < cardRankings[string(hands[j][n])]
 					break
 				}
 			}
@@ -98,6 +117,7 @@ func main() {
 		})
 	}
 
+	// Calculate total
 	total := 0
 	rank := 1
 	for _, handType := range handCount {
@@ -107,4 +127,8 @@ func main() {
 		}
 	}
 	fmt.Println(total)
+}
+
+func main() {
+	partOne()
 }
